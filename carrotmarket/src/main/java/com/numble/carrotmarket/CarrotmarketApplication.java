a8d998b6cf4feb8a79f0d4063fe42fec4a5f3df2
dinner-time <wpdlxldpf4@gmail.com>
2022-01-09 16:59:55 +0900

    first commit

---
+++ carrotmarket/src/main/java/com/numble/carrotmarket/CarrotmarketApplication.java
@@ -0,0 +1,13 @@
+package com.numble.carrotmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotmarketApplication.class, args);
+	}
+
+}
